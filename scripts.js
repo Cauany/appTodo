335d97cb204eb781894ebd795df2b476a263c487
Cauany Souza <souza.cauany99@gmail.com>
2020-05-26 11:41:20 -0300

    criada funções de renderizar, deletar e adicionar

---
+++ scripts.js
@@ -0,0 +1,49 @@
+const listElement = document.querySelector('#app ul')
+const inputElement = document.querySelector('#app input')
+const buttonElement = document.querySelector('#app button')
+
+const todos = [
+    'Fazer café',
+    'Estudar Javascript',
+    'Acessar comunidade da Rocketseat'
+]
+
+function renderTodos(){
+    listElement.innerHTML = ''
+
+    for(todo of todos){
+        const todoElement = document.createElement('li')
+        const todoText = document.createTextNode(todo)
+
+        const linkElement = document.createElement('a')
+        const linkText = document.createTextNode('Excluir')
+
+        const pos = todos.indexOf(todo)
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos +')')
+
+        linkElement.appendChild(linkText)
+        linkElement.setAttribute('href', '#')
+
+        todoElement.appendChild(todoText)
+        todoElement.appendChild(linkElement)
+
+        listElement.appendChild(todoElement)
+    }
+}
+renderTodos()
+
+
+function addTodo(){
+    let todoText = inputElement.value
+
+    todos.push(todoText)
+    inputElement.value = ''
+    renderTodos()
+}
+
+buttonElement.onclick = addTodo
+
+function deleteTodo(pos){
+    todos.splice(pos, 1)
+    renderTodos()
+}
